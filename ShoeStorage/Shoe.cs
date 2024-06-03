5fa14325d87eed664096f687533e1bec2f6674b2
Gusabavo <gustavo.sk.caxias@gmail.com>
2024-06-03 16:04:59 -0300

    Adicionar arquivos de projeto.

---
+++ ShoeStorage/Shoe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeStorage
+{
+    class Shoe
+    {
+        public Style Style { get; private set; }
+        public string Color { get; private set; }
+        public Shoe(Style style, string color)
+        {
+            Style = style;
+            Color = color;
+        }
+        public string Description { get { return $"A {Color} {Style}"; } } 
+    }
+}
